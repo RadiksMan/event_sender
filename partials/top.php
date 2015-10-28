57177588b2b7f765b47c6aa98a35d383612fe5f0
Radiks <radion91@gmail.com>
2015-10-28 17:59:08 +0200

    top 20%

---
+++ partials/top.php
@@ -0,0 +1,17 @@
+<div class="top_section">
+    <div class="top_section_wrap">
+        <div class="top_section_slider">
+            <div class="top_section_slider_wrap">
+                <div class="top_section_item">
+                    <p>Принимайте звонки,</p>
+                    <p>письма и заказы</p>
+                    <p><b>уже сегодня!</b></p>
+                </div>
+            </div>
+        </div>
+        <div class="top_section_middle">
+
+        </div>
+
+    </div>
+</div>

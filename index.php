0d2f88ebadb7c0cdc217cc1bde7802bf34123fa0
ArtGarm <artem.garmatyk@gmail.com>
2015-10-28 17:29:30 +0200

    add fonts

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
+<div class="main mbox">
+	<section class="content_w">
+		<div class="content">
+			<?partial('testcontent')?>
 
-	<div class="main mbox">
-		<section class="content_w">
-			<div class="content">
-				<?partial('testcontent')?>
+		</div>
+	</section>
+	<aside class="sidebar_w">
+		<div class="sidebar"></div>
+	</aside>
+	<div class="clear"></div>
 
-			</div>
-		</section>
-		<aside class="sidebar_w">
-			<div class="sidebar"></div>
-		</aside>
-		<div class="clear"></div>
+<div class="main ">
+	<?partial('top')?>
+	<?partial('algoritm')?>
+	<?partial('servise')?>
 
-	<div class="main ">
-		<?partial('top')?>
-		<?partial('algoritm')?>
-		<?partial('servise')?>
-
-	</div>
+</div>

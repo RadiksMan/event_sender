863e504ad893ddfd00f0c367819edde9ace3c3c8
Radiks <radion91@gmail.com>
2015-10-28 16:21:53 +0200

    no message

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-	<div class="main mbox">
-		<section class="content_w">
-			<div class="content">
-				<?partial('testcontent')?>
-			</div>
-		</section>
-		<aside class="sidebar_w">
-			<div class="sidebar"></div>
-		</aside>
-		<div class="clear"></div>
+	<div class="main ">
+		<?partial('top')?>
+		<?partial('algoritm')?>
+		<?partial('servise')?>
 	</div>

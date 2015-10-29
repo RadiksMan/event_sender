90e5902a2a3baf1e4dab8b94102b892769bc37cc
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-29 12:32:53 +0200

    no message

--- partials/algoritm.php
+++ partials/algoritm.php
@@ -10,14 +10,14 @@
 						<img src="images/bodyimg.png" alt=""/>
 					</div>
 				</div>
-				<div class="algoritm-list">
+				<div class="algoritm-list animate-section">
 					<ul>
-						<li class="li01"><span>01</span><p>Вы выбираете, оплачиваете тариф</p></li>
-						<li class="li02"><span>02</span><p>Вы высылаете материалы (тексты/фото) и пожелания по рассылке</p></li>
-						<li class="li03"><span>03</span><p>Мы разрабатываем вам красивый макет для рассылки БЕСПЛАТНО</p></li>
-						<li class="li04"><span>04</span><p>Мы проводим рассылку в оговоренное время</p></li>
-						<li class="li05"><span>05</span><p>Вы получаете отчет и статистику о рассылке</p></li>
-						<li class="li06"><span>06</span><p>Вы принимаете звонки, письма и заказы от клиентов</p></li>
+						<li class="li01 animate-delay"><span>01</span><p>Вы выбираете, оплачиваете тариф</p></li>
+						<li class="li02 animate-delay"><span>02</span><p>Вы высылаете материалы (тексты/фото) и пожелания по рассылке</p></li>
+						<li class="li03 animate-delay"><span>03</span><p>Мы разрабатываем вам красивый макет для рассылки БЕСПЛАТНО</p></li>
+						<li class="li04 animate-delay"><span>04</span><p>Мы проводим рассылку в оговоренное время</p></li>
+						<li class="li05 animate-delay"><span>05</span><p>Вы получаете отчет и статистику о рассылке</p></li>
+						<li class="li06 animate-delay"><span>06</span><p>Вы принимаете звонки, письма и заказы от клиентов</p></li>
 					</ul>
 				</div>
 			</div>

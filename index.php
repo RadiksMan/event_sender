9881717721513c4c9a70fa03cf2bfcf9081f0303
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-29 10:43:38 +0200

    no message

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-	<div class="main mbox">
-		<section class="content_w">
-			<div class="content">
-				<?partial('testcontent')?>
-			</div>
-		</section>
-		<aside class="sidebar_w">
-			<div class="sidebar"></div>
-		</aside>
-		<div class="clear"></div>
-	</div>
+<?partial('algoritm')?>

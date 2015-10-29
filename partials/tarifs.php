8eb31b6d492e38ab9fbecb6cfb1976dbbfcd5c02
Dmitry <dmitry1940@mail.ru>
2015-10-29 15:05:08 +0200

    added tarifs markup

--- partials/tarifs.php
+++ partials/tarifs.php
@@ -1,5 +1,120 @@
 <div class="tarifs">
     <div class="tarifs-wrap mbox">
+        <div class="tarifs-title-main">
+            тарифы
+        </div>
+        <div class="tarifs-wrap-main">
 
+            <div class="tarifs-item">
+                <div class="tarifs-title">
+                    Инвесторы
+                </div>
+                <div class="tarifs-prices-wrap">
+                    <div class="tarifs-price">
+                        <div class="tarifs-price-title">
+                            Москва/МО
+                        </div>
+                        <div class="tarifs-price-num">
+                            <p>9</p>
+                            <p>тыс</p>
+                        </div>
+                    </div>
+                    <div class="tarifs-price">
+                        <div class="tarifs-price-title">
+                            Вся Россия
+                        </div>
+                        <div class="tarifs-price-num">
+                            <p>14</p>
+                            <p>тыс</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="tarifs-description">
+                    <div class="tarifs-description-item">
+                        Event-агентства и организаторы HR/PR сотрудники крупных компаний
+                    </div>
+                </div>
+                <div class="tarifs-button">
+                    <a href="#" class="button">Заказать</a>
+                </div>
+            </div>
+
+            <div class="tarifs-item">
+                <div class="tarifs-title">
+                    Площадки
+                </div>
+                <div class="tarifs-prices-wrap">
+                    <div class="tarifs-price">
+                        <div class="tarifs-price-title">
+                            Москва/МО
+                        </div>
+                        <div class="tarifs-price-num">
+                            <p>7</p>
+                            <p>тыс</p>
+                        </div>
+                    </div>
+                    <div class="tarifs-price">
+                        <div class="tarifs-price-title">
+                            Вся Россия
+                        </div>
+                        <div class="tarifs-price-num">
+                            <p>11</p>
+                            <p>тыс</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="tarifs-description">
+                    <div class="tarifs-description-item">
+                        Арт- директора и менеджеры площадок
+                    </div>
+                </div>
+                <div class="tarifs-button">
+                    <a href="#" class="button">Заказать</a>
+                </div>
+            </div>
+
+            <div class="tarifs-item">
+                <div class="tarifs-title">
+                    Инвесторы+Площадки
+                </div>
+                <div class="tarifs-prices-wrap">
+                    <div class="tarifs-price">
+                        <div class="tarifs-price-title">
+                            Москва/МО
+                        </div>
+                        <div class="tarifs-price-num">
+                            <p>12.5</p>
+                            <p>тыс</p>
+                        </div>
+                    </div>
+                    <div class="tarifs-price">
+                        <div class="tarifs-price-title">
+                            Вся Россия
+                        </div>
+                        <div class="tarifs-price-num">
+                            <p>19.5</p>
+                            <p>тыс</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="tarifs-description">
+                    <div class="tarifs-description-item">
+                        Рассылка по всей базе
+                    </div>
+                </div>
+                <div class="tarifs-button">
+                    <a href="#" class="button">Заказать</a>
+                </div>
+            </div>
+
+            <!--<div class="tarifs-item">
+
+            </div>
+
+            <div class="tarifs-item">
+
+            </div>-->
+
+        </div>
     </div>
 </div>
